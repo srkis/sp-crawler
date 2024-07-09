edcaca6008038aa29a601fb21750e89e7ff68e45
Srdjan <stojanovicsrdjan27@gmail.com>
2024-07-09 14:42:48 +0200

    Create sp-crawler-public-display.php

---
+++ public/partials/sp-crawler-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://github.com/srkis
+ * @since      1.0.0
+ *
+ * @package    SP_Crawler
+ * @subpackage SP_Crawler/includes
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
